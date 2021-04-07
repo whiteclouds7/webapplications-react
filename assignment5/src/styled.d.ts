13254b73841f00febb195907c7d8c0317d85f243
whiteclouds7 <christina.albertani@students.fhv.at>
2021-04-07 18:46:52 +0200

    assignment 5 - fixed dark theme error with Default Theme

--- assignment5/src/styled.d.ts
+++ assignment5/src/styled.d.ts
@@ -2,5 +2,5 @@ import { themes } from "./App";
 
 declare module "styled-components" {
   type CustomTheme = typeof themes.light;
-  export type DefaultTheme = CustomTheme;
+  export interface DefaultTheme extends CustomTheme {}
 }
