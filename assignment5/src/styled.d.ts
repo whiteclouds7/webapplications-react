888d4174fc567474e320967efa948f63de61c126
whiteclouds7 <65771894+whiteclouds7@users.noreply.github.com>
2021-04-11 20:55:52 +0200

    assignment 4 - fixed es-lint problem

--- assignment5/src/styled.d.ts
+++ assignment5/src/styled.d.ts
@@ -2,5 +2,12 @@ import { themes } from "./App";
 
 declare module "styled-components" {
   type CustomTheme = typeof themes.light;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends CustomTheme {}
+
+  // the other solution if I don't disable eslint but the one above is nicer i think
+  // export interface DefaultTheme extends CustomTheme {
+  //   backgroundColor: string;
+  //   color: string;
+  // }
 }
